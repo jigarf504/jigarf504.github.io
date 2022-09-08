dd77bdfc80f09ae30673d77486a07ccd131e9eac
Jigar Kumar <jigarf504@gmail.com>
2022-09-08 19:48:49 +0530

    changes in index html and added script js

---
+++ script.js
@@ -0,0 +1,75 @@
+new Vue({
+  el: "#myApp",
+  data: {
+    technologies: {
+      columnOne: [
+        {
+          url: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg",
+          title: "HTML5",
+        },
+        {
+          url: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-plain.svg",
+          title: "CSS3",
+        },
+        {
+          url: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bootstrap/bootstrap-plain.svg",
+          title: "BootStrap",
+        },
+        {
+          url: "https://www.vectorlogo.zone/logos/tailwindcss/tailwindcss-icon.svg",
+          title: "Tailwind CSS",
+        },
+      ],
+      columnTwo: [
+        {
+          url: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg",
+          title: "Javascript",
+        },
+        {
+          url: "https://uxwing.com/wp-content/themes/uxwing/download/brands-and-social-media/vue-js-icon.svg",
+          title: "Vuejs",
+        },
+        {
+          url: "https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original-wordmark.svg",
+          title: "React",
+        },
+        {
+          url: "https://raw.githubusercontent.com/devicons/devicon/master/icons/redux/redux-original.svg",
+          title: "Redux",
+        },
+      ],
+      columnThree: [
+        {
+          url: "https://raw.githubusercontent.com/devicons/devicon/master/icons/nodejs/nodejs-original-wordmark.svg",
+          title: "NodeJs",
+        },
+        {
+          url: "https://raw.githubusercontent.com/devicons/devicon/master/icons/express/express-original-wordmark.svg",
+          title: "Express",
+        },
+        {
+          url: "https://cdn.worldvectorlogo.com/logos/laravel-2.svg",
+          title: "Laravel",
+        },
+        {
+          url: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mysql/mysql-plain.svg",
+          title: "MySql",
+        },
+      ],
+      columnFour: [
+        {
+          url: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-plain.svg",
+          title: "MongoDB",
+        },
+        {
+          url: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-plain.svg",
+          title: "Git",
+        },
+        {
+          url: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/heroku/heroku-plain.svg",
+          title: "Heroku",
+        },
+      ],
+    },
+  },
+});
